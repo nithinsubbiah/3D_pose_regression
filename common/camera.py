b797a6e5db45eb4944842e9cc9b9a3e5b574a5cb
Nithin <nithinsubbiah@gmail.com>
2019-12-10 02:34:54 -0500

    added cuda() to variables in camera.py

--- common/camera.py
+++ common/camera.py
@@ -47,17 +47,17 @@ def project_to_2d(X, camera_params):
     while len(camera_params.shape) < len(X.shape):
         camera_params = camera_params.unsqueeze(1)
 
-    f = camera_params[..., :2]
-    c = camera_params[..., 2:4]
-    k = camera_params[..., 4:7]
-    p = camera_params[..., 7:]
+    f = camera_params[..., :2].cuda()
+    c = camera_params[..., 2:4].cuda()
+    k = camera_params[..., 4:7].cuda()
+    p = camera_params[..., 7:].cuda()
 
-    XX = torch.clamp(X[..., :2] / X[..., 2:], min=-1, max=1)
-    r2 = torch.sum(XX[..., :2] ** 2, dim=len(XX.shape) - 1, keepdim=True)
+    XX = torch.clamp(X[..., :2] / X[..., 2:], min=-1, max=1).cuda()
+    r2 = torch.sum(XX[..., :2] ** 2, dim=len(XX.shape) - 1, keepdim=True).cuda()
 
     radial = 1 + torch.sum(k * torch.cat((r2, r2 ** 2, r2 ** 3), dim=len(r2.shape) - 1), dim=len(r2.shape) - 1,
-                           keepdim=True)
-    tan = torch.sum(p * XX, dim=len(XX.shape) - 1, keepdim=True)
+                           keepdim=True).cuda()
+    tan = torch.sum(p * XX, dim=len(XX.shape) - 1, keepdim=True).cuda()
 
     XXX = XX * (radial + tan) + p * r2
 
